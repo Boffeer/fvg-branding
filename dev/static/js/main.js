e44b8e64e125e0763a48acf2c5f0db8b2b936699
boffeer <boffeechane@gmail.com>
2020-11-13 16:30:41 +0300

    another fixes

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -14,23 +14,23 @@ $(document).ready(function () {
 
 
 
-    $('.hero-slider').slick({
+    $('.js-footer-slider').slick({
     	centerMode: true,
 		// centerPadding: '144px',
     	slidesToShow: 1,
-        slidesToScroll: 1,
+      slidesToScroll: 1,
     	infinite: true,
     	dots: true,
     	arrows: true,
-        swipeToSlide: true,
-    	nextArrow: '<svg class="slick-next" width="63" height="63" viewBox="0 0 63 63" fill="none" xmlns="http://www.w3.org/2000/svg"><path opacity="0.78" d="M28.2844 3L56.5687 31.2843L28.2844 59.5685" stroke="white" stroke-width="8"/></svg>',
-	    prevArrow: '<svg class="slick-prev" width="63" height="63" viewBox="0 0 63 63" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M34.2843 3L6.00003 31.2843L34.2843 59.5685" stroke="white" stroke-width="8"/></svg>',
-        responsive:[{
-            breakpoint: 480,
-            settings: {
-                centerMode: false,
-                arrows: false
-            },
+      swipeToSlide: true,
+      nextArrow: '<svg class="slick-next" width="89" height="89" viewBox="0 0 89 89" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M37.6534 77.9108L68.7661 46.7981C70.0353 45.5289 70.0353 43.4711 68.7661 42.2019L37.6534 11.0892C36.5795 10.0153 36.5795 8.27406 37.6534 7.20012L42.5555 2.2981C43.6294 1.22416 45.3706 1.22416 46.4445 2.2981L86.7019 42.5555C87.7758 43.6294 87.7758 45.3706 86.7019 46.4445L46.4445 86.7019C45.3706 87.7758 43.6294 87.7758 42.5555 86.7019L37.6534 81.7999C36.5795 80.7259 36.5795 78.9847 37.6534 77.9108Z" fill="white" stroke="#232323" stroke-width="1"/></svg>',
+      prevArrow: '<svg class="slick-prev" width="89" height="89" viewBox="0 0 89 89" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M51.3466 11.0892L20.2339 42.2019C18.9647 43.4711 18.9647 45.5289 20.2339 46.7981L51.3466 77.9108C52.4205 78.9847 52.4205 80.7259 51.3466 81.7999L46.4445 86.7019C45.3706 87.7758 43.6294 87.7758 42.5555 86.7019L2.2981 46.4445C1.22416 45.3706 1.22416 43.6294 2.2981 42.5555L42.5555 2.2981C43.6294 1.22416 45.3706 1.22416 46.4445 2.2981L51.3466 7.20012C52.4205 8.27406 52.4205 10.0153 51.3466 11.0892Z" fill="white" stroke="#232323" stroke-width="1"/></svg>',
+      responsive:[{
+          breakpoint: 480,
+          settings: {
+              centerMode: false,
+              arrows: false
+          },
         }],
     })
 
@@ -49,9 +49,16 @@ $(document).ready(function () {
 
 
 
+window.addEventListener('scroll', function() {
+  if (pageYOffset > 100){
+    $('.burger').addClass('burger--scrolled');
+    $('.menu-burger_close').addClass('burger--scrolled');
 
-
-
+  } else {
+    $('.menu-burger_close').removeClass('burger--scrolled');
+    $('.burger').removeClass('burger--scrolled');
+  }
+});
 
 
 
@@ -136,6 +143,19 @@ $(document).ready(function () {
       }
     ]
   });
+  $('.work_btn--cta').click(function(){
+    var formName = $(this).data('formname');
+    $('.g-form-name').val(formName).change();
+    $('.hero-cta-modal_wrap').fadeIn();
+    $('html').addClass('modal-open');
+  });
+  $('.work_btn--contacts').click(function(){
+    var formName = $(this).data('formname');
+    $('.g-form-name').val(formName).change();
+    $('.modal-contacts_wrap').fadeIn();
+    $('html').addClass('modal-open');
+  });
+
 
 
   AOS.init();
@@ -143,12 +163,20 @@ $(document).ready(function () {
   $('.services-item__btn').click(function(){
     var service = $(this).data('service')
     $('.g-form-name').val(service).change();
-    $('.services-modal_wrap').css({'display': 'flex'})
+    // $('.services-modal_wrap').css({'display': 'flex'})
+    $('.hero-cta-modal_wrap').fadeIn()
+    $('html').addClass('modal-open');
   })
 
   $('.modal_close-btn').click(function(){
-    $('.services-modal_wrap').css({'display': 'none'})
-    $('.hero-modal_wrap').css({'display': 'none'})
+    // $('.services-modal_wrap').css({'display': 'none'})
+    // $('.hero-modal_wrap').css({'display': 'none'})    
+    $('.services-modal_wrap').fadeOut();
+    $('.hero-modal_wrap').fadeOut();
+    $('.hero-cta-modal_wrap').fadeOut();
+    $('.modal-contacts_wrap').fadeOut();
+    $('html').removeClass('modal-open');
+
     // $('.case-modal_wrap').css({'display': 'none'})
     if ($('.case-modal').hasClass('js-zoomer')){
       $('.case-modal').toggleClass('js-zoomer')
@@ -156,24 +184,44 @@ $(document).ready(function () {
     }  // $('.case-modal_wrap').css({'display': 'none'})
   })
 
+
+
+  $('.hero-cta').click(function(){
+    $('.hero-cta-modal_wrap').fadeIn();
+    $('html').addClass('modal-open');
+  });
+
   $('.hero_callback').click(function(){
     $('.g-form-name').val('Перезвонить с главной').change()
-    $('.hero-modal_wrap').css({'display': 'flex'})
+    // $('.hero-modal_wrap').css({'display': 'flex'});
+    $('.hero-modal_wrap').fadeIn();
+    $('html').addClass('modal-open');
   })
 
   $('.menu-burger_close').click(function(){
     $('.menu_wrap').slideToggle()
+    $('html').removeClass('modal-open');
   })
+
   $('.menu_nav-item').click(function(){
     $('.menu_wrap').slideToggle()
+    $('html').removeClass('modal-open');
   })
+  $('.hero-menu-logo--inside').click(function(){
+    $('.menu_wrap').slideToggle()
+    $('html').removeClass('modal-open');
+  })
+
+
   $('.burger').click(function(){
     $('.menu_wrap').slideToggle()
+    $('html').addClass('modal-open');
   })
 
   $('.cases-item__btn').click(function(){
     var thisCase = $(this).data('case-number')
 
+    $('html').addClass('modal-open');
     $('.case-modal--'+thisCase).fadeToggle()
     $('.case-modal--'+thisCase).css({'display': 'flex'})
     // $('.case-modal--'+thisCase+' .case-modal').css({'transform': 'scale(1)'})
@@ -196,6 +244,70 @@ $(document).ready(function () {
       counterCounter = 1;
     }
   })
+
+
+// $('.menu_nav-item').mouseover(function(){
+//   if ($('.menu_nav-item').hasClass('menu_nav-item--active')){
+//     $(this).removeClass('menu_nav-item--active');
+//   } else {
+//     $(this).addClass('menu_nav-item--active');
+//   }
+// });
+
+
+
+
+$('.case-modal').scroll(function() {
+    var y = $('.case-modal').scrollTop();
+    $(".case-modal .modal_close-btn").css('top', y+50);
+});
+
+
+
+
+
+// closing beyond popup
+
+$(document).mouseup(function (e){
+  var modalctr = $(".case-modal_wrap");
+  var modal = $(".case-modal");
+  if (!modal.is(e.target) && modal.has(e.target).length === 0){
+    modalctr.fadeOut();
+  }
+  $('html').removeClass('modal-open');  
+});
+
+$(document).mouseup(function (e){
+  var modalctr = $(".hero-modal_wrap");
+  var modal = $(".hero-modal");
+  if (!modal.is(e.target) && modal.has(e.target).length === 0){
+    modalctr.fadeOut();
+  }
+  $('html').removeClass('modal-open');  
+});
+
+$(document).mouseup(function (e){
+  var modalctr = $(".hero-cta-modal_wrap");
+  var modal = $(".hero-modal");
+  if (!modal.is(e.target) && modal.has(e.target).length === 0){
+    modalctr.fadeOut();
+  }
+  $('html').removeClass('modal-open');  
+});
+
+$(document).mouseup(function (e){
+  var modalctr = $(".modal-contacts_wrap");
+  var modal = $(".hero-modal");
+  if (!modal.is(e.target) && modal.has(e.target).length === 0){
+    modalctr.fadeOut();
+  }
+  $('html').removeClass('modal-open');  
+});
+
+
+
+
+
 
 
 // --- End Ready
